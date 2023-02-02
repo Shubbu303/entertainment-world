9492c5b612a2494155624ecb3744550ce7692ae1
Shubham Kumar <abdshubham13@gmail.com>
2023-02-02 08:49:39 +0000

    Update Login.js

--- src/component/Login/Login.js
+++ src/component/Login/Login.js
@@ -1,41 +1,42 @@
-import React from 'react';
-import { useNavigate } from 'react-router-dom';
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
-import './Login.css';
+import "./Login.css";
 const Login = () => {
-	const navigate = useNavigate();
-	// {TODO: Setup firebase for login and store login data in redux}
-	return (
-		<div className='login'>
-			<div className='login__form'>
-				<h2>Login</h2>
-				<div className='login__email'>
-					<input type='text' placeholder='Email' />
-				</div>
-				<div className='login__password'>
-					<input type='password' placeholder='Password' />
-				</div>
-				<div className='login__button'>
-					<button
-						onClick={() => {
-							console.log('btn');
-						}}
-					>
-						Login
-					</button>
-				</div>
-				<div className='login__signup'>
-					<button
-						onClick={() => {
-							navigate('/signup');
-						}}
-					>
-						Don't have Account? Sign up
-					</button>
-				</div>
-			</div>
-		</div>
-	);
+  const navigate = useNavigate();
+  // {TODO: Setup firebase for login and store login data in redux}
+  const [email, setEmail] = useState();
+  return (
+    <div className="login">
+      <div className="login__form">
+        <h2>Login</h2>
+        <div className="login__email">
+          <input type="text" placeholder="Email" />
+        </div>
+        <div className="login__password">
+          <input type="password" placeholder="Password" />
+        </div>
+        <div className="login__button">
+          <button
+            onClick={() => {
+              console.log("btn");
+            }}
+          >
+            Login
+          </button>
+        </div>
+        <div className="login__signup">
+          <button
+            onClick={() => {
+              navigate("/signup");
+            }}
+          >
+            Don't have Account? Sign up
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
